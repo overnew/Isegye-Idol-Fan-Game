18e0b85e8abb6f6a4d9d2161806e3313667be23f
EVEerNew <ehdls1303@gmail.com>
2022-02-09 20:09:33 +0900

    Feat: graph 노드 연결성 구현
    - json파일에서 인접 노드를 가져와 그래프 구현

---
+++ Assets/Script/Stage/EdgeData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class EdgeData
+{
+    [SerializeField] public string[] edges;
+}
---
+++ Assets/Script/Stage/GraphMaker.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+
+public class GraphMaker : MonoBehaviour
+{
+    const string graphDataPath = "DataBase/StageGraph";
+    private EdgeData edgeData;
+    private List<Node> graph = new List<Node>();
+
+    public List<Node> GetLineGraph(string graphType)
+    {
+        LoadGraphDataFromJson(graphType);
+        MakeGraph();
+        return graph;
+    }
+
+    private void LoadGraphDataFromJson(string graphType)
+    {
+        string path = Path.Combine(Application.dataPath, graphDataPath, graphType);
+        string jsonData = File.ReadAllText(path);
+        edgeData = JsonUtility.FromJson<EdgeData>(jsonData);
+    }
+    private void MakeGraph()
+    {
+        for (int idx=0; idx <edgeData.edges.Length ; ++idx)
+        {
+            graph.Add(new Node(idx, edgeData.edges[idx]));
+        }
+    }
+}
+
+public class Node
+{
+    private int nodeNumber;
+    private List<int> nextNodes = new List<int>();
+
+    public Node(int _nodeNumber, string adjacentEdges)
+    {
+        this.nodeNumber = _nodeNumber;
+
+        List<string> tempList = new List<string>();
+        tempList.AddRange(adjacentEdges.Split('.'));
+
+        for (int i=0; i<tempList.Count ; ++i)
+            nextNodes.Add(Int32.Parse(tempList[i]));
+    }
+
+}
