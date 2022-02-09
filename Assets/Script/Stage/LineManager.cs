58727152213ccb044cd1944f4b7e83445cc39894
EVEerNew <ehdls1303@gmail.com>
2022-02-09 17:50:30 +0900

    Feat: 특정 포인트의 최대 개수 제한
    - cafe, random 포인트의 개수 제한 생성 구현

--- Assets/Script/Stage/LineManager.cs
+++ Assets/Script/Stage/LineManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class LineManager : MonoBehaviour
 {
     const int StageNameLength = 5;
+    private int cafeCount = 0, randomCount = 0;
+
     public Sprite[] pointImages;
     private SpriteRenderer[] points;
 
@@ -25,21 +27,49 @@ public class LineManager : MonoBehaviour
 
         for (int i=2; i<points.Length-1 ; ++i)
         {
-            int stageIdx = Random.Range(0, StageNameLength-1);
+            int stageIdx;
+
+            do
+            {
+                stageIdx = Random.Range(1, StageNameLength - 1);
+            } while (!CheckUnderMaxPoints(stageIdx));
+            
             stageList.Add((StageName)stageIdx);
             points[i].sprite = pointImages[stageIdx];
+
+            if (stageIdx == (int)StageName.cafe)
+                ++cafeCount;
+            else if (stageIdx == (int)StageName.random)
+                ++randomCount;
         }
 
         points[points.Length -1].sprite = pointImages[(int)StageName.treasure];
         stageList.Add(StageName.treasure);
     }
+    private bool CheckUnderMaxPoints(int stageIdx)
+    {
+        if (stageIdx == (int)StageName.cafe && (int)MaxPoint.cafe <= cafeCount)
+            return false;
+
+        if (stageIdx == (int)StageName.random && (int)MaxPoint.random <= randomCount)
+            return false;
+
+        return true;
+    }
 }
 
-public enum StageName
+
+enum StageName
 {
     start, 
     battle,
     cafe,
-    question,
+    random,
     treasure
+}
+
+enum MaxPoint
+{
+    cafe = 2,
+    random = 3
 }
